caaf883edcabd189906f36fcab173e3014f2ec80
Justyn Ooi <justynooi@hotmail.com>
2020-07-07 14:45:28 +0800

    Parent + Child items complete

--- admin/scripts/scripts.js
+++ admin/scripts/scripts.js
@@ -12,7 +12,7 @@ function getCookie(name) {
 var token = getCookie("webapitoken");
 var baseURL = window.location.hostname;
 adminId = document.getElementById("userGuid").value;
-var merchantID, stockQuantity, button, stockAdd, children, total;
+var merchantID, stockQuantity, button, stockAdd, children, total, variant, variantItem, variantItemID, submit;
 
 function incrementStock(itemID) {
     var object;
@@ -32,17 +32,19 @@ function incrementStock(itemID) {
         console.log("has children");
         children = object.ChildItems;
         console.log(children);
-        var input = "<br>";
+        var input = "<br>\n";
         for (var i = 1; i <= children.length; i++) {
-            var variant = "variant" + i
-            input = input + "<div>\n" + "<input type=\"radio\" id=\"" + variant + "\" name=\"" + variant + "\">\n"
-            input = input + "<label for=\"" + variant + "\">" + children[i-1].Variants[0].Name + "</label>\n" + "</div><br>\n"
+            variant = "variant" + i
+            input = input + "<div>\n" + "<input type=\"radio\" id=\"" + variant + "\" name=\"variant\" value=\"" + children[i-1].ID + "\">\n"
+            input = input + "<label for=\"" + variant + "\">" + children[i - 1].Variants[0].Name + "</label>\n" + "</div><br>\n"
         }
-        input = input + "<button id=\"submitVariant\" onclick=\"submitVariant()\">Submit</button>"
+        input = input + "<button id=\"variantID\" onclick=\"submitVariantID()\">Submit</button>"
         var popup = document.getElementById("popup");
         popup.innerHTML = input;
         popup.classList.add("show");
         popup.classList.remove("hide");
+        submit = document.getElementById("submitID");
+        submit.classList.add("hide");
     } else {
         console.log("no children");
         data = {
@@ -69,5 +71,44 @@ function submitID() {
     stockAdd = document.getElementById("number").value;
     incrementStock(itemID);
 }
+
+function submitVariantID() {
+    for (var i = 1; i <= children.length; i++) {
+        variant = "variant" + i
+        if(document.getElementById(variant).checked) {
+            variantItem = document.getElementById(variant);
+            variantItemID = variantItem.value;
+            console.log(variantItemID);
+            for (var i = 0; i < children.length; i++) {
+                if (variantItemID == children[i].ID) {
+                    stockQuantity = children[i].StockQuantity;
+                }
+            }
+            data = {
+                StockQuantity: Number(stockQuantity) + Number(stockAdd)
+            }
+            settings = {
+                url: "https://" + baseURL + "/api/v2/merchants/" + merchantID + "/items/" + variantItemID,
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: "Bearer " + token
+                },
+                data: JSON.stringify(data),
+                async: false
+            }
+            $.ajax(settings).done(function(response) {
+                console.log("Done!");
+            });
+            break;
+        }
+    }
+    submit.classList.remove("hide");
+    popup.classList.add("hide");
+    popup.classList.remove("show");
+}
+
+
+
 //83d9c73f-8b2f-4f36-a7bc-1b4c81f3bb90
 //18d7a59b-4344-4cb6-b672-5b6602cf0068
